68ed2039fdd3d0b98d07437c72d9264ca9253de2
Brandon Foong <brandonfoongyankai@gmail.com>
2021-02-05 17:43:19 +0800

    Add in-lecture improvements to the code

---
+++ src/main/java/circus/Trainer.java
@@ -0,0 +1,36 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
+public class Trainer {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+        getToSpeak(d);
+
+        Bird b = (Bird)d;  // upcasting
+        getToSpeak(b);
+
+        Animal a = (Animal)b; // upcasting (redundant - implicitly added by compiler)
+        getToSpeak(a);
+
+        Duck d2 = (Duck) a; // downcasting (needs to be stated explicitly, add instanceof as a guard)
+        train(new Duck());
+        train(new Parrot());
+        // Animal a2 = new Animal();
+        // Bird b2 = new Bird();
+    }
+
+    private static void getToSpeak(Animal animal) {
+        System.out.println(animal.speak());
+    }
+
+    private static void train(Bird bird) {
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        }
+    }
+}
